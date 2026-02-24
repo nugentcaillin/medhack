3a25ce8b02393d1f6b1e0591e4cdd8b618b59fe2
Caillin Nugent <caillinn@student.unimelb.edu.au>
2026-02-24 12:06:58 +1100

    skeleton for 3d sole card

--- frontend/app/checkout/page.tsx
+++ frontend/app/checkout/page.tsx
@@ -1,7 +1,36 @@
+import { Card, CardDescription, CardHeader, CardTitle   } from "@/components/ui/card"
+import "./checkoutStyle.css"
+
+
+function CheckoutWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    return (
+        <div className="checkoutWrapper">
+            {children}
+        </div>
+    )
+}
+
+
+function SoleModelDisplayer() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Sole Model</CardTitle>
+                <CardDescription></CardDescription>
+            </CardHeader>
+        </Card>
+    )
+}
+
+
 export default function Home() {
   return (
-    <div>
-        Hello, World
-    </div>
+    <CheckoutWrapper>
+        <SoleModelDisplayer />
+    </CheckoutWrapper>
   )
 }
